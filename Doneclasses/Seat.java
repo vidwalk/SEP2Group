5863e1d299cf57747f741466a34c4b92afe69b8b
vidwalk <claudiurediu@gmail.com>
2018-04-19 12:34:47 +0200

    seat enum fixed

--- Doneclasses/Seat.java
+++ Doneclasses/Seat.java
@@ -1,65 +1,23 @@
 package ZAir.domain.model;
 
 public enum Seat {
-A1,
-B1,
-C1,
-A2,
-B2,
-C2,
-A3,
-B3,
-C3,
-A4,
-B4,
-C4,
-A5,
-B5,
-C5,
-A6,
-B6,
-C6,
-A7,
-B7,
-C7,
-A8,
-B8,
-C8,
-A9,
-B9,
-C9,
-A10,
-B10,
-C10,
-A11,
-B11,
-C11,
-A12,
-B12,
-C12,
-A13,
-B13,
-C13,
-A14,
-B14,
-C14,
-A15,
-B15,
-C15,
-A16,
-B16,
-C16,
-A17,
-B17,
-C17,
-A18,
-B18,
-C18,
-A19,
-B19,
-C19,
-A20,
-B20,
-C20,;
 
+	A1(false), B1(false), C1(false), A2(false), B2(false), C2(false), A3(false), B3(false), C3(false), A4(false), B4(
+			false), C4(false), A5(false), B5(false), C5(false), A6(false), B6(false), C6(false), A7(false), B7(
+					false), C7(false), A8(false), B8(false), C8(false), A9(false), B9(false), C9(
+							false), A10(false), B10(false), C10(false), A11(false), B11(false), C11(false), A12(
+									false), B12(false), C12(false), A13(false), B13(false), C13(false), A14(false), B14(
+											false), C14(false), A15(false), B15(false), C15(false), A16(false), B16(
+													false), C16(false), A17(false), B17(false), C17(false), A18(
+															false), B18(false), C18(false), A19(false), B19(false), C19(
+																	false), A20(false), B20(false), C20(false);
+	private boolean seatValue;
+
+	private Seat(boolean value) {
+		seatValue = value;
+	}
+
+	public void Book() {
+		seatValue = true;
+	}
 }

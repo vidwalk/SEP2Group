4b74e548d849f36891285e32103ae928f366b10c
vidwalk <claudiurediu@gmail.com>
2018-05-28 14:45:48 +0200

    Database update

--- Sprint1/database/tables.sql
+++ Sprint1/database/tables.sql
@@ -1,24 +1,23 @@
 CREATE TABLE zair."Flight"
 (
-    "flightID" serial NOT NULL,
-    "origin" zair."location",
-    "destination" zair."location",
+    "flightID" varchar(20) NOT NULL,
+    "origin" varchar(30),
+    "destination" varchar(30),
     "dateOfDeparture" zair."Date",
     "dateOfArrival" zair."Date",
     "timeOfDeparture" zair."Time",
     "timeOfArrival" zair."Time",
     "nrOfTickets" zair."numberOfTickets",
-    "nrOfTicketsLeft" zair."numberOfTickets",
     price zair."price",
     PRIMARY KEY ("flightID")
 );
 
 CREATE TABLE zair."Ticket"
 (
-    "ticketID" serial NOT NULL,
-    "flightID" serial NOT NULL,
-    seat zair."Seat" NOT NULL,
-    "customerID" character(20) NOT NULL,
+    "ticketID" varchar(20) NOT NULL,
+    "flightID" varchar(20) NOT NULL,
+    "seatPo" varchar(4) NOT NULL,
+    "customerID" varchar(20) NOT NULL,
     PRIMARY KEY ("ticketID", "flightID"),
     UNIQUE("ticketID"),
     CONSTRAINT "flightID" FOREIGN KEY ("flightID")
@@ -28,23 +27,39 @@ CREATE TABLE zair."Ticket"
     CONSTRAINT "customerID" FOREIGN KEY ("customerID")
     REFERENCES zair."Customer" ("customerID") MATCH SIMPLE
     ON UPDATE NO ACTION
+    ON DELETE NO ACTION,
+    CONSTRAINT "seatPo" FOREIGN KEY ("seatPo", "flightID")
+    REFERENCES zair."Seats" ("seatPo", "flightID") MATCH SIMPLE
+    ON UPDATE NO ACTION
     ON DELETE NO ACTION
 );
 CREATE TABLE zair."Customer"
 (
-    "customerID" character(20) NOT NULL,
-    "fName" zair."Name" NOT NULL,
-    "lName" zair."Name" NOT NULL,
-    email zair."email",
-    "passportNo" character(15),
-    "phone" character(15),
+    "customerID" varchar(20) NOT NULL,
+    "fName" varchar(20)  NOT NULL,
+    "lName" varchar(20)  NOT NULL,
+    email varchar(18),
+    "passportNo" varchar(15),
+    "phone" varchar(15),
     PRIMARY KEY ("customerID")
 );
 CREATE TABLE zair."CustomerCredentials"
 (
-    "customerID" character(20) NOT NULL,
-    "userID" character(20) NOT NULL,
-    password zair."password" NOT NULL,
+    "customerID" varchar(20) NOT NULL,
+    "userID" varchar(20) NOT NULL,
+    password varchar(24) NOT NULL,
     FOREIGN KEY ("customerID")
         REFERENCES zair."Customer" ("customerID") MATCH SIMPLE
+);
+CREATE TABLE zair."Seats"
+(
+    "seatPo" varchar(4) NOT NULL,
+    "seatValue" boolean NOT NULL,
+    "flightID" varchar(20) NOT NULL,
+    
+    CONSTRAINT "flightID" FOREIGN KEY ("flightID")
+        REFERENCES zair."Flight" ("flightID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+        PRIMARY KEY ("seatPo","flightID")
 );
